65d96f854f0f4a1136386bc1a47c4936d49c15e8
Debraj Banerjee <155657741+Seven-Indians@users.noreply.github.com>
2024-11-25 11:33:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), 'modules'))
 
 import login # type: ignore
+import home # type: ignore
 
 def main():
     login.main()
+    home.main()
 
 if __name__ == "__main__":
     main()
